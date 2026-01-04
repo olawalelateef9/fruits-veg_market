601944e0d8f7c08405379382e8f57711a7fb2f4e
olawale lateef <olawalelateef9@gmail.com>
2026-01-04 01:55:29 -1200

    main.tf updated to include jenkins_tool_server creation

--- terraform/main.tf
+++ terraform/main.tf
@@ -81,7 +81,52 @@ resource "aws_security_group" "web_sg" {
         Name = "web-sg"
     }
 }
+# Jenkins Security Group
+resource "aws_security_group" "jenkins_sg" {
+    name   = "jenkins-sg"
+    vpc_id = var.vpc_id
 
+    ingress {
+        from_port   = 22
+        to_port     = 22
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    # Jenkins web UI (8080)
+    ingress {
+        from_port   = 8080
+        to_port     = 8080
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags = {
+        Name = "jenkins-sg"
+    }
+}
+
+# Jenkins EC2 instance for project tooling
+resource "aws_instance" "project_tool_server" {
+    count                      = 1
+    ami                        = var.backend_ami
+    instance_type              = var.instance_type
+    subnet_id                  = aws_subnet.private_subnet_1.id
+    vpc_security_group_ids     = [aws_security_group.jenkins_sg.id]
+    key_name                   = var.key_name
+    associate_public_ip_address = false
+
+    tags = {
+        Name = "project_tool_server"
+    }
+}
 # Backend Security Group
 resource "aws_security_group" "backend_sg" {
     name        = "backend-sg"
@@ -99,6 +144,13 @@ resource "aws_security_group" "backend_sg" {
         to_port         = 8000
         protocol        = "tcp"
         security_groups = [aws_security_group.web_sg.id]
+    }
+
+    ingress {
+        from_port       = 8000
+        to_port         = 8000
+        protocol        = "tcp"
+        security_groups = [aws_security_group.jenkins_sg.id]
     }
 
     ingress {
